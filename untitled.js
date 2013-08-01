702e1a97d507b3f832f070864c35330cb8d79f25
musmic@gmail.com <musmic@gmail.com>
2013-08-01 17:19:13 -0400

    Hi

---
+++ untitled.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+(function() {
+if ( currentUser.tags.indexOf('reseller') >= 0 ) {
+        	document.getElementById("reseller-link").style.display = "";
+        }
+        else{
+            document.getElementById("reseller-link").style.display = "none";
+        }
+}
+}());
+</script>
